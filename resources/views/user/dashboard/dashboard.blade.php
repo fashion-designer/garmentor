9ba9a51e3ef26b76567c4d2ce8d0c3874aeedb3d
fashion-designer <hireafashiondesigner@gmail.com>
2019-12-08 21:42:44 -0500

    Admin:User: remove dashboard view default text

--- resources/views/user/dashboard/dashboard.blade.php
+++ resources/views/user/dashboard/dashboard.blade.php
@@ -9,7 +9,6 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        You are logged in!
                     </div>
                 </div>
             </div>
