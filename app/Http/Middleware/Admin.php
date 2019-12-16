3fbd3767752223b4fc0f282be6f065595bbab0a8
fashion-designer <hireafashiondesigner@gmail.com>
2019-12-16 00:24:05 -0500

    Admin: redirect on active/verified

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -15,12 +15,23 @@ class Admin
      */
     public function handle($request, Closure $next)
     {
-        if(!auth()->guard('admin')->check()
-            || auth()->guard('admin')->user()->is_active !== 1
-            || auth()->guard('admin')->user()->is_verified !== 1)
+        if (!auth()->guard('admin')->check())
         {
-            auth()->guard('admin')->logout();
             return redirect('admin/login');
+        }
+        elseif (auth()->guard('admin')->check())
+        {
+            if(auth()->guard('admin')->user()->is_active !== 1)
+            {
+                auth()->guard('admin')->logout();
+                return redirect('admin/login');
+            }
+
+            if(auth()->guard('admin')->user()->is_verified !== 1)
+            {
+                auth()->guard('admin')->logout();
+                return redirect(route('send-verification-admin'));
+            }
         }
 
         return $next($request);
